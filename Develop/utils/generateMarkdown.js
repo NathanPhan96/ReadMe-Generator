de8091a1f5e33c5e9633d6b4dd536ec73284a4c8
Nathan Phan <nathanphan96@yahoo.com>
2022-11-29 02:48:03 -0800

    added code to generateMarkdown and index.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,8 +12,25 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.fileName}
+  ## License and built with
+${data.license ? data.license : ''} ${data.made ? data.made : ''}
+## The Title of the application
+${data.title}
+## Description
+ ${data.description}
+ ## Installation Instructions
+ ${data.installation}
+ ## Usage
+ ${data.usage}
+ ## Contribution
+ ${data.contributors}
+ ## Tests
+ ${data.test}
 
+ ## Questions
+If you have any questions you can reach out to me at ${data.github}
+or at ${data.email}.
 `;
 }
 
